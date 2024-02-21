5cff73d21e666692a12fb3a5df1cc95423fa3080
Dozgulbas <dozgulbas@anl.gov>
2024-02-20 19:02:08 -0600

    Update;Working on the new locations

--- platecrane_driver/platecrane_driver.py
+++ platecrane_driver/platecrane_driver.py
@@ -1,7 +1,7 @@
 import json
 import re
 
-from platecrane_driver.serial_port import SerialPort
+from serial_port import SerialPort
 
 
 class PlateCrane:
@@ -959,6 +959,8 @@ if __name__ == "__main__":
     Runs given function.
     """
     s = PlateCrane("/dev/ttyUSB2")
+    # s.initialize()
+    # s.home()
     stack4 = "Stack4"
     stack5 = "Stack5"
     solo6 = "Solo.Position6"
@@ -967,12 +969,28 @@ if __name__ == "__main__":
     target_loc = "HidexNest2"
     lidnest3 = "LidNest3"
     sealer = "SealerNest"
+    # s.move_location("Safe")
+    # s.pick_stack_plate("Stack1")  
+    # a = s.get_position()
+    # s.set_location("LidNest3",R=231449,Z=-31500,P=484,Y=-306)
+
+    # s.set_location("Hidex.Nest",R=a[0],Z=a[1],P=a[2],Y=a[3])
+    # s.place_module_plate("Hidex.Nest")
+    # s.move_single_axis("Z","Hidex.Nest")
+    s.transfer("Hidex.Nest","Solo.Position1",source_type="module",target_type="stack",height_offset=800)
+    # s.transfer("Stack1", "PeelerNest",source_type="stack",target_type="stack")
+
+    # s.place_module_plate()
     # s.get_location_list()
+
+    # s.move_joints_neutral()
+    # s.move_single_axis("R", "Safe", delay_time=1)
+    # s.set_location("Safe",R=195399,Z=0,P=0,Y=0)
     # s.set_location("LidNest2",R=131719,Z=-31001,P=-5890,Y=-315)
     # s.transfer(source="LidNest1",target="LidNest2",source_type="stack",target_type="stack", plate_type="96_well")
 
     # s.transfer(source="LidNest2",target="LidNest3",source_type="stack",target_type="stack", plate_type="96_well")
-    # s.pick_stack_plate("LidNest2")
+    # s.transfer("Stack1","Stack1")
     # s.free_joints()
     # s.lock_joints()
 
@@ -993,7 +1011,8 @@ if __name__ == "__main__":
     # s.transfer(solo6, "Stack2", source_type = "module", target_type = "stack", plate_type = "tip_box_lid_on")
 
 
-#    Crash error outputs 21(R axis),14(z axis), 02 Wrong location name. 1400 (Z axis hits the plate), 00 success TODO: Need a response handler function. Unkown error messages T1, ATS, TU these are about connection issues (multiple access?)
+#    Crash error outputs 21(R axis),14(z axis), 02 Wrong location name. 1400 (Z axis hits the plate), 00 success 
+# TODO: Need a response handler function. Unkown error messages T1, ATS, TU these are about connection issues (multiple access?)
 # TODO: Slow the arm before hitting the plate in pick_stack_plate
 # TODO: Create a plate detect function within pick stack plate function
 # TODO: Maybe write another pick stack funtion to remove the plate detect movement
