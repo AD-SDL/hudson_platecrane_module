b439b94efb63c1ad4419d7bebfe8827e263f30c1
Ryan Lewis <accounts+github@ryandlewis.dev>
2024-05-30 12:31:08 -0500

    Testing state assignment

--- src/sciclops_rest_node.py
+++ src/sciclops_rest_node.py
@@ -1,28 +1,23 @@
 #! /usr/bin/env python3
 """The server for the Hudson Platecrane/Sciclops that takes incoming WEI flow requests from the experiment application"""
 
-from contextlib import asynccontextmanager
 from pathlib import Path
-from typing_extensions import Annotated
 
-from platecrane_driver.sciclops_driver import SCICLOPS
-from wei.types.module_types import (
-    ModuleStatus
-    
-)
-from wei.types.step_types import (StepResponse, StepStatus, ActionRequest)
 from fastapi.datastructures import State
-from wei.utils import extract_version
+from platecrane_driver.sciclops_driver import SCICLOPS
+from typing_extensions import Annotated
 from wei.modules.rest_module import RESTModule
-
-
+from wei.types.module_types import ModuleStatus
+from wei.types.step_types import ActionRequest, StepResponse, StepStatus
+from wei.utils import extract_version
 
 rest_module = RESTModule(
-        name="sciclops_node",
-        version=extract_version(Path(__file__).parent.parent / "pyproject.toml"),
-        description="A node to control the sciclops plate moving robot",
-        model="sciclops",
-    )
+    name="sciclops_node",
+    version=extract_version(Path(__file__).parent.parent / "pyproject.toml"),
+    description="A node to control the sciclops plate moving robot",
+    model="sciclops",
+)
+
 
 @rest_module.startup()
 def sciclops_startup(state: State):
@@ -38,9 +33,9 @@ def sciclops_startup(state: State):
 
     try:
         state.sciclops = SCICLOPS()
-        print(dir(state))
+        print(state._state)
     except Exception as error_msg:
-        state.status= ModuleStatus.ERROR
+        state.status = ModuleStatus.ERROR
         print("------- SCICLOPS Error message: " + str(error_msg) + (" -------"))
 
     else:
@@ -54,24 +49,24 @@ def sciclops_startup(state: State):
 def status(state: State, action: ActionRequest):
     return StepResponse(StepStatus.SUCCEEDED, state.sciclops.status())
 
+
 @rest_module.action(name="home", description="force sciclops to check its status")
-def status(state: State, action: ActionRequest):
+def home(state: State, action: ActionRequest):
     state.sciclops.home()
     return StepResponse(StepStatus.SUCCEEDED, "robot homed")
 
 
 @rest_module.action(name="get_plate", description="force sciclops to check its status")
-def status(state: State, 
-           action: ActionRequest,
-           pos: Annotated[int, "Stack to get plate from"],
-           lid: Annotated[bool, "Whether plate has a lid or not"] = False,
-           trash: Annotated[bool, "Whether to use the trash"] = False
-           ):
+def get_plate(
+    state: State,
+    action: ActionRequest,
+    pos: Annotated[int, "Stack to get plate from"],
+    lid: Annotated[bool, "Whether plate has a lid or not"] = False,
+    trash: Annotated[bool, "Whether to use the trash"] = False,
+):
     print(state._state)
     state.sciclops.get_plate(pos, lid, trash)
     return StepResponse(StepStatus.SUCCEEDED, "robot homed")
 
+
 rest_module.start()
-
-
-
